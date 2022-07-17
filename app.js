2d330bed0c144115c2b0be2da8a6297070414a34
Trần Minh Huy <minhhuy9a3dt@gmail.com>
2022-07-17 10:15:47 +0700

    Add get all goods of store

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ db.on("connected", () => console.log("MongoDB connected successfully"));
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
